1804f543eecf4e9b35753913ce1de1ed3d920793
hsimao <e087754958@gmail.com>
2021-04-27 21:49:21 +0800

    feat: keep-alive with page transition setting

---
+++ pages/about.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AboutPage',
+    transition: {
+      name: 'page',
+      mode: ''
+    },
+    mounted() {
+      console.log('About page mounted')
+    },
+    activated() {
+      console.log('About page activated')
+    }
+  }
+</script>
+
+<style scoped>
+.about {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: green;
+  width: 100%;
+  min-height: 100vh;
+}
+</style>
---
+++ pages/profile.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="profile">
+    <h1>Profile</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ProfilePage',
+    transition: {
+      name: 'page',
+      mode: ''
+    },
+    mounted() {
+      console.log('Profile page mounted')
+    },
+    activated() {
+      console.log('Profile page activated')
+    }
+  }
+</script>
+
+<style scoped>
+.profile {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: blue;
+  width: 100%;
+  min-height: 100vh;
+}
+</style>
